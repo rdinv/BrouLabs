5ab9ff6695a2b4af92a50e65cb5139b32baf2202
rdinv <radone21@gmail.com>
2022-02-17 04:33:44 +0200

    mob menu fix

--- build/script/main.js
+++ build/script/main.js
@@ -10,16 +10,33 @@ burgerBtn.addEventListener('click',()=>{
     }
 });
 
-
+const body = document.querySelector('body');
 
 const mobMenu = document.querySelector('.mobMenu-conteiner');
+
+
 let mobMenuOpen = false;
 burgerBtn.addEventListener('click',()=>{
     if(!mobMenuOpen){
         mobMenu.classList.add('open');
+        body.setAttribute("style", "overflow:hidden;");
         mobMenuOpen = true;
     }else{
         mobMenu.classList.remove('open');
+        body.removeAttribute("style", "overflow:hidden;");
         mobMenuOpen = false;
     }
+});
+
+
+const mobMenuHypers = document.querySelectorAll('.a-mob-hypers');
+
+[].forEach.call(mobMenuHypers, function(el){
+    el.addEventListener('click', function (e) {
+        if(mobMenuOpen == true){
+            mobMenu.classList.remove('open');
+            body.removeAttribute("style", "overflow:hidden;");
+            mobMenuOpen = false;
+        }
+    });
 });
