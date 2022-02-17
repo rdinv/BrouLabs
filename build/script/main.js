4a75b4250df16703806f84687332701be9658cc5
rdinv <radone21@gmail.com>
2022-02-17 17:06:09 +0200

    mob menu fix

--- build/script/main.js
+++ build/script/main.js
@@ -1,42 +1,34 @@
-const burgerBtn = document.querySelector('.burger-menu__btn');
+const body = document.querySelector("body");
+
+const mobMenu = document.querySelector(".mobMenu-conteiner");
+const mobMenuHypers = document.querySelectorAll(".mobLinks");
+const burgerBtn = document.querySelector(".burger-menu__btn");
 let menuOpen = false;
-burgerBtn.addEventListener('click',()=>{
-    if(!menuOpen){
-        burgerBtn.classList.add('open');
-        menuOpen = true;
-    }else{
-        burgerBtn.classList.remove('open');
-        menuOpen = false;
-    }
+
+burgerBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    burgerBtn.classList.add("open");
+    mobMenu.classList.add("open");
+    body.setAttribute("style", "overflow:hidden;");
+    menuOpen = true;
+  } else {
+    burgerBtn.classList.remove("open");
+    mobMenu.classList.remove("open");
+    body.removeAttribute("style", "overflow:hidden;");
+    menuOpen = false;
+  }
 });
 
-const body = document.querySelector('body');
 
-const mobMenu = document.querySelector('.mobMenu-conteiner');
-
-
-let mobMenuOpen = false;
-burgerBtn.addEventListener('click',()=>{
-    if(!mobMenuOpen){
-        mobMenu.classList.add('open');
-        body.setAttribute("style", "overflow:hidden;");
-        mobMenuOpen = true;
-    }else{
-        mobMenu.classList.remove('open');
+[].forEach.call(mobMenuHypers, function (el) {
+    el.addEventListener("click", function (e) {
+      if (menuOpen == true) {
+        mobMenu.classList.remove("open");
         body.removeAttribute("style", "overflow:hidden;");
-        mobMenuOpen = false;
-    }
-});
-
-
-const mobMenuHypers = document.querySelectorAll('.a-mob-hypers');
-
-[].forEach.call(mobMenuHypers, function(el){
-    el.addEventListener('click', function (e) {
-        if(mobMenuOpen == true){
-            mobMenu.classList.remove('open');
-            body.removeAttribute("style", "overflow:hidden;");
-            mobMenuOpen = false;
-        }
+        burgerBtn.classList.remove("open");
+        menuOpen = false;
+      }
     });
-});
+  });
+
+
